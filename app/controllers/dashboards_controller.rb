4c9843b0aed7aa27a35a16d5966e2b3292f11a2e
thomas julien <thomas.julien@skema.edu>
2019-12-12 15:22:14 +0100

    fixed iterations in dashboard and landing page + added avatar to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,15 +1,12 @@
 class DashboardsController < ApplicationController
   def index
     @bookings = current_user.bookings
-    @bookings.each { |booking| @booking = booking }
     @offices = current_user.offices
     @bookings_received = []
     @offices.each do |office|
-      @office = office
-      @office.bookings.each do |booking|
+      office.bookings.each do |booking|
         @bookings_received << booking
       end
     end
-    @booking.reviews.each { |review| @review = review }
   end
 end
