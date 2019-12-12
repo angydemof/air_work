899acdc76559ae909348e5a62b6b7499ad675959
Angelica de Moffarts <angelica.demoffarts@gmail.com>
2019-12-12 14:44:07 +0100

    WIP: on flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,6 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
 import { initMapbox } from '../plugins/init_mapbox';
+import "../plugins/flatpickr"
 
 initMapbox();
-
-$("#calendar").flatpickr({
-    mode: 'range',
-    dateFormat: "Y-m-d"
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  flatpickr('#calendar');
-})
