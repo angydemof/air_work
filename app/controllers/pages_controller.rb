1a547779317ef73180515d5ed1c4a8ad8c791f45
thomas julien <thomas.julien@skema.edu>
2019-12-12 16:10:12 +0100

    End of landing page before Css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @offices = Office.all
+    @offices = Office.all.first(10)
   end
 end
