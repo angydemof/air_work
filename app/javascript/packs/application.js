b2fff2559c828de98661ed80851bcf4df368026d
thomas julien <thomas.julien@skema.edu>
2019-12-16 17:06:38 +0100

    End of office new page beofre ending styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the styleshe
 import { initMapbox } from '../plugins/init_mapbox';
 
 import { initFlatpickr } from '../plugins/flatpickr';
+import { schedule } from '../offices/schedule_choice';
 
 initMapbox();
 initFlatpickr();
+schedule();
