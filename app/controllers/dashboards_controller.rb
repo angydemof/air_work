027c70f87a3ce40c8c733a93abaacd4ae78a287b
thomas julien <thomas.julien@skema.edu>
2019-12-17 10:40:53 +0100

    fixed seed and sorted results in the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,13 @@
 class DashboardsController < ApplicationController
   def index
-    @bookings = current_user.bookings
-    @offices = current_user.offices
+    @bookings = current_user.bookings.order(created_at: :desc)
+    @offices = current_user.offices.order(created_at: :desc)
     @bookings_received = []
     @offices.each do |office|
       office.bookings.each do |booking|
         @bookings_received << booking
       end
     end
+    @bookings_received.sort_by(&:created_at).reverse
   end
 end
